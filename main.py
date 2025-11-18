8821e013b9d18cffa35ea3370acbf3a86ebd175a
pygamegrupo-eng <pygamegrupo@gmail.com>
2025-11-17 22:00:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ black=(0,0,0)
 Fps=60
 
 #pj
+Suelo_Y = Alto - 50
 player_size = 40      
 player_x = 100       
-player_y = 300       
+player_y = Suelo_Y - player_size
 player_color = (255, 255, 0)  
+color_piso = (104, 106, 217)
+
 
 pygame.init()
 pygame.display.set_caption(" ")
@@ -29,6 +32,7 @@ while ejecutando:
 
   screen.fill(black)
 
+  pygame.draw.rect(screen, color_piso ,(0, Suelo_Y, Ancho, 50))
   pygame.draw.rect(screen, player_color, (player_x, player_y, player_size, player_size))
 
     
